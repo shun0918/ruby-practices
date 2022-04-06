be9ae1899e2da759eb956f62568d9b4feb289998
shun0918 <nihei@tebiki.co.jp>
2022-04-06 18:32:03 +0900

    リファクタ: rubocopが通るように

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require 'date'
 
 MIN_YEAR = 1970
 MAX_YEAR = 2100
 MIN_MONTH = 1
 MAX_MONTH = 12
-OPTION_KEY = {MONTH: "-m", YEAR: "-y"}
-TODAY = Date.today
+OPTION_KEY = { month: '-m', year: '-y' }.freeze
+TODAY = Date.today.freeze
 
 def validate_year(year)
   MIN_YEAR <= year && year <= MAX_YEAR
@@ -23,57 +25,69 @@ def error_message(str)
   "\e[31mERROR: #{str}\e[0m"
 end
 
+def show_calendar_head(date)
+  puts "     #{date.strftime('%-m月 %Y')}"
+  puts '日 月 火 水 木 金 土  '
+end
+
+def show_calendar_week(weeks: [])
+  weeks.each do |day|
+    day == TODAY.day.to_s.rjust(2, ' ') ? print(bg_red(day)) : print(day)
+    print(' ')
+  end
+  puts
+end
+
 # 年月をもとにカレンダー出力
 def show_calendar(year:, month:)
+  raise '年の値が不正です' unless validate_year(year)
+  raise '月の値が不正です' unless validate_month(month)
+
   head = Date.new(year, month, 1)
   tail = Date.new(year, month, -1)
-  weekTable = []
-  weekRow = Array.new(7, "  ")
+  weeks = []
+  tmp_week = Array.new(7, '  ')
 
   (head..tail).each do |date|
-    weekRow[date.wday] = date.day.to_s.rjust(2, " ")
+    tmp_week[date.wday] = date.day.to_s.rjust(2, ' ')
     if date.wday == 6
-      weekTable.push weekRow
-      weekRow = Array.new(7, "  ")
+      weeks.push tmp_week
+      tmp_week = Array.new(7, '  ')
     end
   end
 
-  puts "     #{head.strftime("%-m月 %Y")}"
-  puts "日 月 火 水 木 金 土  "
-  weekTable.each do |row|
-    row.each do |day|
-      day == TODAY.day.to_s.rjust(2, " ") ? print(bg_red(day)) : print(day)
-      print(" ")
+  show_calendar_head(head)
+  weeks.each { |week| show_calendar_week(week) }
+end
+
+def read_calendar_options
+  options = { OPTION_KEY[:month] => TODAY.month, OPTION_KEY[:year] => TODAY.year }
+  current_option = nil
+
+  ARGV.each do |arg|
+    if current_option.nil?
+      raise "不正なオプションです: オプション: #{arg}" unless options.key?(arg.to_s)
+
+      current_option = arg.to_s
+      next
     end
-    puts()
+
+    raise "オプションに対する値が不正です。 オプション: #{current_option}, 値: #{arg}" unless arg.to_s.match?(/^[0-9]+$/)
+
+    options[current_option] = arg.to_i
+    current_option = nil
   end
+
+  raise "#{current_option}オプションの後には引数が必要です" unless current_option.nil?
+
+  { year: options[OPTION_KEY[:year]], month: options[OPTION_KEY[:month]] }
 end
 
 def main
-  begin
-    options = { OPTION_KEY[:MONTH] => TODAY.month, "-y" => TODAY.year }
-    current_option = nil
-    
-    ARGV.each_with_index do |arg, i|
-      if current_option.nil?
-        raise "不正なオプションです: オプション: #{arg.to_s}" unless options.has_key?(arg.to_s)
-        current_option = arg.to_s
-        next
-      end
-
-      raise "オプションの値が不正です。 オプション: #{current_option}, 値: #{arg.to_s}" unless arg.to_s =~ /^[0-9]+$/
-      options[current_option] = arg.to_i
-      current_option = nil
-    end
-
-    raise "#{current_option} オプションの後には引数が必要です" unless current_option.nil?
-    raise "年の値が不正です" unless validate_year(options["-y"])
-    raise "月の値が不正です" unless validate_month(options[OPTION_KEY[:MONTH]])
-
-    show_calendar(year: options["-y"], month: options[OPTION_KEY[:MONTH]])
-  rescue => e
-    puts error_message(e.message)
-  end
+  inputed_date = read_calendar_options
+  show_calendar(year: inputed_date[:year], month: inputed_date[:month])
+rescue StandardError => e
+  puts error_message(e.message)
 end
 
 main
