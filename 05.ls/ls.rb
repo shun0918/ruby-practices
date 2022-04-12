ee497212d882251b63cbb33ef85ecb5b403ab9d6
shun0918 <nihei@tebiki.co.jp>
2022-04-12 09:17:58 +0900

    パス指定可能に

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,21 +1,27 @@
 # frozen_string_literal: true
 
 COLMUN = 3
+DEFAULT_PATH = './'
 
-def print_item(item)
-  print "#{item}  "
+def print_item(item, path)
+  print "#{item.slice(path.length..)}  "
 end
 
-def show_dir
-  Dir.glob('*').each_with_index do |item, index|
+def show_dir(path)
+  Dir.glob("#{path}*").each_with_index do |item, index|
     puts if index.positive? && (index % COLMUN).zero?
-    print_item(item)
+    print_item(item, path)
   end
   puts
 end
 
+def trailing_slash(path)
+  path.end_with?('/') ? path : "#{path}/"
+end
+
 def ls
-  show_dir
+  path = !ARGV[0].nil? && !ARGV[0].empty? ? trailing_slash(ARGV[0]) : DEFAULT_PATH
+  show_dir(path)
 end
 
 ls
